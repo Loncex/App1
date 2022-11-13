6607aad8b34eee291efdf80e3b8c6755d3c0ae93
Loncex <silvacriollo@gmail.com>
2022-11-13 17:20:30 -0400

    Version 1: 13/11/2022 menu

---
+++ app/src/main/java/com/example/app1/Principal1.java
@@ -0,0 +1,34 @@
+package com.example.app1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class Principal1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal1);
+
+        TabLayout tl = (TabLayout) findViewById(R.id.tablayout);
+        tl.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+}
